37d12a7f14a928278ab33313aba5b3af18eeef28
balaji <balajinia@gmail.com>
2022-01-09 11:07:25 +0530

    here we again

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+
+const { useRef } = React;
 
 function App() {
+  const pageRefs = useRef({});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header pageRefs={pageRefs} />
+      <About pageRefs={pageRefs} />
+      <Technology pageRefs={pageRefs} />
+      <Portfolio pageRefs={pageRefs} />
     </div>
+  );
+}
+
+function Header({ pageRefs }) {
+  function scrollIntoView(type) {
+    pageRefs.current[type].scrollIntoView({ behavior: "smooth" });
+  }
+
+  return (
+    <header className="navbar">
+      <button onClick={() => scrollIntoView("about")}>Product</button>
+      <button onClick={() => scrollIntoView("techno")}>Service</button>
+      <button onClick={() => scrollIntoView("portfolio")}>Contact</button>
+    </header>
+  );
+}
+
+function About({ pageRefs }) {
+  return (
+    <section
+      className="page about"
+      ref={(el) => (pageRefs.current = { ...pageRefs.current, about: el })}
+    >
+      <span>Product</span>
+    </section>
+  );
+}
+
+function Technology({ pageRefs }) {
+  return (
+    <section
+      className="page techno"
+      ref={(el) => (pageRefs.current = { ...pageRefs.current, techno: el })}
+    >
+      <span>Service</span>
+    </section>
+  );
+}
+
+function Portfolio({ pageRefs }) {
+  return (
+    <section
+      className="page portfolio"
+      ref={(el) => (pageRefs.current = { ...pageRefs.current, portfolio: el })}
+    >
+      <span>Contact</span>
+    </section>
   );
 }
 
